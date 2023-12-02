5ea486be18c1f95b48c67f1a0e55e336dd161903
zhuikovda <dmitiy.zhuikov@janix-soft.com>
2023-12-02 16:49:58 +0300

    feat: finish HW

--- android/app/src/main/kotlin/com/example/esd_corp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.esd_corp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:esd_corp/esd_main_screen/esd_main_screen.dart';
+import 'package:esd/esd_main_screen/esd_main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
